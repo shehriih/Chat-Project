d2b7a9ee39cb1eb6fd7dfaf6acde64e09680563a
Ibrahim ALShehri <ibrahim.alshehri@sv.cmu.edu>
2011-04-13 11:34:35 -0700

    Added status and name in first row

--- Chat/gen/com/android/chat/R.java
+++ Chat/gen/com/android/chat/R.java
@@ -8,10 +8,18 @@
 package com.android.chat;
 
 public final class R {
+    public static final class array {
+        public static final int statusArray=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+    }
+    public static final class id {
+        public static final int name=0x7f060001;
+        public static final int root=0x7f060000;
+        public static final int status=0x7f060002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -19,5 +27,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int nameText=0x7f040002;
+        public static final int status_prompt=0x7f040003;
     }
 }
