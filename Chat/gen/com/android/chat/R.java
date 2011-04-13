f523d35545548aaf07f592047d23404516bc054f
Ibrahim ALShehri <ibrahim.alshehri@sv.cmu.edu>
2011-04-13 13:42:23 -0700

    UI 0.1

--- Chat/gen/com/android/chat/R.java
+++ Chat/gen/com/android/chat/R.java
@@ -9,6 +9,7 @@ package com.android.chat;
 
 public final class R {
     public static final class array {
+        public static final int TestList=0x7f050001;
         public static final int statusArray=0x7f050000;
     }
     public static final class attr {
@@ -17,17 +18,37 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int name=0x7f060001;
+        public static final int add_button=0x7f060005;
+        public static final int add_friend_title=0x7f060001;
+        public static final int add_row=0x7f060002;
+        public static final int chatList=0x7f060007;
+        public static final int chat_box=0x7f06000a;
+        public static final int chat_prompt=0x7f060009;
+        public static final int contactList=0x7f06000e;
+        public static final int email_label=0x7f060003;
+        public static final int email_text_edit=0x7f060004;
+        public static final int last_row=0x7f060008;
+        public static final int name=0x7f06000c;
         public static final int root=0x7f060000;
-        public static final int status=0x7f060002;
+        public static final int send=0x7f06000b;
+        public static final int status=0x7f06000d;
+        public static final int title=0x7f060006;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int add_friend=0x7f030000;
+        public static final int chat_session=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
+        public static final int add_button=0x7f040006;
+        public static final int add_friend_title=0x7f040009;
         public static final int app_name=0x7f040001;
+        public static final int chat_box_prompt=0x7f040005;
+        public static final int email_hint=0x7f040008;
+        public static final int email_label=0x7f040007;
         public static final int hello=0x7f040000;
         public static final int nameText=0x7f040002;
         public static final int status_prompt=0x7f040003;
+        public static final int titleText=0x7f040004;
     }
 }
--- Chat/src/com/android/chat/ChatSession.java
+++ Chat/src/com/android/chat/ChatSession.java
@@ -3,15 +3,14 @@ package com.android.chat;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class chat extends Activity
+public class ChatSession extends Activity
 {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
+		 setContentView(R.layout.chat_session);
 
 	}
 }
